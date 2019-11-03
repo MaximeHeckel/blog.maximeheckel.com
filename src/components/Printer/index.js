5dc27994e70622ac93d6df180fbfb6bc569ac8dc
Maxime <heckelmaxime@gmail.com>
2019-11-03 13:56:28 -0800

    Improve Printer component

--- src/components/Printer/index.js
+++ src/components/Printer/index.js
@@ -2,43 +2,25 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 const Wrapper = styled('div')`
-  background: #ffffff;
-  padding: 50px;
   width: 800px;
   height: 400px;
   color: #2b2d3e;
-  font-family: -apple-system, 'BlinkMacSystemFont', 'Segoe UI', 'Roboto',
-    'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
+  font-family: 'Helvetica', sans-serif;
   display: flex;
-  flex-direction: column;
-  justify-content: center;
-`;
-
-const Content = styled('div')`
-  display: flex;
-  align-items: center;
-  height: 200px;
-
+  background: #fff;
   svg {
-    max-width: 90px;
-    min-width: 90px;
+    max-width: 200px;
+    min-width: 200px;
   }
 `;
 
 const Title = styled('h1')`
-  margin-left: 20px;
-  margin-top: 0px;
-  margin-bottom: 0px;
+  padding-left: 20px;
+  margin-top: 30px;
+  margin-bottom: 20px;
   font-size: 38px;
   font-weight: 500;
   line-height: 1.5;
-`;
-
-const Footer = styled('div')`
-  display: flex;
-  justify-content: flex-end;
-  padding-right: 50px;
 `;
 
 const LocalLogo = () => (
@@ -50,7 +32,7 @@ const LocalLogo = () => (
     xmlns="http://www.w3.org/2000/svg"
   >
     <g>
-      <circle cx="200.633" cy="200.58" r="200" fill="#2B2D3E" />
+      <circle cx="200.633" cy="200.58" r="200" fill="none" />
       <rect
         x="131.283"
         y="104.174"
@@ -58,7 +40,7 @@ const LocalLogo = () => (
         height="144.938"
         rx="23.2779"
         transform="rotate(26 131.283 104.174)"
-        fill="#FFFFFF"
+        fill="#fff"
       />
       <rect
         x="231.663"
@@ -67,7 +49,7 @@ const LocalLogo = () => (
         height="144.938"
         rx="23.2779"
         transform="rotate(26 231.663 109.137)"
-        fill="#FFFFFF"
+        fill="#fff"
       />
       <rect
         x="257.779"
@@ -76,7 +58,7 @@ const LocalLogo = () => (
         height="68.1965"
         rx="23.2779"
         transform="rotate(-30 257.779 207.753)"
-        fill="#FFFFFF"
+        fill="#fff"
       />
     </g>
   </svg>
@@ -84,13 +66,22 @@ const LocalLogo = () => (
 
 const PrinterComponent = ({ title }) => (
   <Wrapper>
-    <Content>
+    <div
+      style={{ backgroundColor: '#2b2d3e', maxWidth: '250px', height: '100%' }}
+    >
       <LocalLogo />
+    </div>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+      }}
+    >
       <Title>{title}</Title>
-    </Content>
-    <Footer>
-      <h3 style={{ color: '#196FD8' }}>@MaximeHeckel</h3>
-    </Footer>
+      <h3 style={{ color: '#196FD8', paddingLeft: '20px' }}>@MaximeHeckel</h3>
+    </div>
+    {/*  */}
   </Wrapper>
 );
 
