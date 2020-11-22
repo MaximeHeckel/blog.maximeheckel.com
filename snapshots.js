f51d4cf858979a6a9df7ed1423e2c70005ad901f
Maxime Heckel <heckelmaxime@gmail.com>
2020-11-22 17:57:10 -0500

    chore(dependency): Bump theme dependency to get new dedup webmention component (#234)

--- snapshots.js
+++ snapshots.js
@@ -1,123 +1,24 @@
 module.exports = {
-  "SEO test": {
-    "has all the meta tags in the landing page head": {
-      "1": [
-        "<meta charSet=\"utf-8\"/",
-        "<meta http-equiv=\"x-ua-compatible\" content=\"ie=edge\"/",
-        "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\"/",
-        "<meta name=\"generator\" content=\"Gatsby 2.21.11\"/",
-        "<meta data-react-helmet=\"true\" name=\"description\" content=\"Hi I&#x27;m Maxime, I write about React, Redux, JAMStack, Typescript, GraphQL, Serverless, Testing and many other things!\"/",
-        "<meta data-react-helmet=\"true\" name=\"image\" content=\"\"/",
-        "<meta data-react-helmet=\"true\" property=\"og:url\" content=\"https://blog.maximeheckel.com/\"/",
-        "<meta data-react-helmet=\"true\" name=\"keywords\" content=\"Software Engineer, React, Redux, Typescript, Jest, Kubernetes, Docker, Entreprise, Frontend, Engineering, Blog\"/",
-        "<meta data-react-helmet=\"true\" property=\"og:title\" content=\"Maxime Heckel&#x27;s Blog\"/",
-        "<meta data-react-helmet=\"true\" property=\"og:description\" content=\"Hi I&#x27;m Maxime, I write about React, Redux, JAMStack, Typescript, GraphQL, Serverless, Testing and many other things!\"/",
-        "<meta data-react-helmet=\"true\" name=\"twitter:card\" content=\"summary",
-        "<meta data-react-helmet=\"true\" name=\"twitter:creator\" content=\"@MaximeHeckel\"/",
-        "<meta data-react-helmet=\"true\" name=\"twitter:site\" content=\"@MaximeHeckel\"/",
-        "<meta data-react-helmet=\"true\" name=\"twitter:title\" content=\"Maxime Heckel&#x27;s Blog\"/",
-        "<meta data-react-helmet=\"true\" name=\"twitter:description\" content=\"Hi I&#x27;m Maxime, I write about React, Redux, JAMStack, Typescript, GraphQL, Serverless, Testing and many other things!\"/",
-        "<meta name=\"theme-color\" content=\"#ffffff\"/"
-      ],
-      "2": [
-        "rel=\"canonical\" href=\"https://blog.maximeheckel.com/\"/"
-      ]
-    },
-    "has all the meta tags in the blog post head": {
-      "1": [
-        "<meta charSet=\"utf-8\"/",
-        "<meta http-equiv=\"x-ua-compatible\" content=\"ie=edge\"/",
-        "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\"/",
-        "<meta name=\"generator\" content=\"Gatsby 2.21.11\"/",
-        "<meta data-react-helmet=\"true\" name=\"description\" content=\"A guide to get started with AST (Abstract Syntax Tree) and custom built ESLint plugins\"/",
-        "<meta data-react-helmet=\"true\" name=\"image\" content=\"https://blog.maximeheckel.com/opengraph-images/how-i-built-my-first-custom-esl-int-rule.png\"/",
-        "<meta data-react-helmet=\"true\" property=\"og:url\" content=\"https://blog.maximeheckel.com/posts/how-to-build-first-eslint-rule/\"/",
-        "<meta data-react-helmet=\"true\" property=\"og:type\" content=\"article\"/",
-        "<meta data-react-helmet=\"true\" name=\"keywords\" content=\"Software Engineer, React, Redux, Typescript, Jest, Kubernetes, Docker, Entreprise, Frontend, Engineering, Blog\"/",
-        "<meta data-react-helmet=\"true\" property=\"og:title\" content=\"How I built my first custom ESLint rule - Maxime Heckel&#x27;s Blog\"/",
-        "<meta data-react-helmet=\"true\" property=\"og:description\" content=\"A guide to get started with AST (Abstract Syntax Tree) and custom built ESLint plugins\"/",
-        "<meta data-react-helmet=\"true\" property=\"og:image\" content=\"https://blog.maximeheckel.com/opengraph-images/how-i-built-my-first-custom-esl-int-rule.png\"/",
-        "<meta data-react-helmet=\"true\" name=\"twitter:card\" content=\"summary",
-        "<meta data-react-helmet=\"true\" name=\"twitter:creator\" content=\"@MaximeHeckel\"/",
-        "<meta data-react-helmet=\"true\" name=\"twitter:site\" content=\"@MaximeHeckel\"/",
-        "<meta data-react-helmet=\"true\" name=\"twitter:title\" content=\"How I built my first custom ESLint rule - Maxime Heckel&#x27;s Blog\"/",
-        "<meta data-react-helmet=\"true\" name=\"twitter:description\" content=\"A guide to get started with AST (Abstract Syntax Tree) and custom built ESLint plugins\"/",
-        "<meta data-react-helmet=\"true\" name=\"twitter:image\" content=\"https://blog.maximeheckel.com/opengraph-images/how-i-built-my-first-custom-esl-int-rule.png\"/",
-        "<meta name=\"theme-color\" content=\"#ffffff\"/"
-      ],
-      "2": [
-        "rel=\"canonical\" href=\"https://blog.maximeheckel.com/posts/how-to-build-first-eslint-rule/\"/"
-      ]
-    }
-  },
-  "__version": "5.4.0",
   "SEO: Verify meta tag integrity": {
-    "has all the meta tags in the landing page head": {
-      "1": [
-        "<meta charSet=\"utf-8\"/",
-        "<meta http-equiv=\"x-ua-compatible\" content=\"ie=edge\"/",
-        "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\"/",
-        "<meta name=\"generator\" content=\"Gatsby 2.21.11\"/",
-        "<meta data-react-helmet=\"true\" name=\"description\" content=\"Hi I&#x27;m Maxime, I write about React, Redux, JAMStack, Typescript, GraphQL, Serverless, Testing and many other things!\"/",
-        "<meta data-react-helmet=\"true\" name=\"image\" content=\"\"/",
-        "<meta data-react-helmet=\"true\" property=\"og:url\" content=\"https://blog.maximeheckel.com/\"/",
-        "<meta data-react-helmet=\"true\" name=\"keywords\" content=\"Software Engineer, React, Redux, Typescript, Jest, Kubernetes, Docker, Entreprise, Frontend, Engineering, Blog\"/",
-        "<meta data-react-helmet=\"true\" property=\"og:title\" content=\"Maxime Heckel&#x27;s Blog\"/",
-        "<meta data-react-helmet=\"true\" property=\"og:description\" content=\"Hi I&#x27;m Maxime, I write about React, Redux, JAMStack, Typescript, GraphQL, Serverless, Testing and many other things!\"/",
-        "<meta data-react-helmet=\"true\" name=\"twitter:card\" content=\"summary",
-        "<meta data-react-helmet=\"true\" name=\"twitter:creator\" content=\"@MaximeHeckel\"/",
-        "<meta data-react-helmet=\"true\" name=\"twitter:site\" content=\"@MaximeHeckel\"/",
-        "<meta data-react-helmet=\"true\" name=\"twitter:title\" content=\"Maxime Heckel&#x27;s Blog\"/",
-        "<meta data-react-helmet=\"true\" name=\"twitter:description\" content=\"Hi I&#x27;m Maxime, I write about React, Redux, JAMStack, Typescript, GraphQL, Serverless, Testing and many other things!\"/",
-        "<meta name=\"theme-color\" content=\"#ffffff\"/"
-      ],
-      "2": [
-        "rel=\"canonical\" href=\"https://blog.maximeheckel.com/\"/"
-      ]
-    },
-    "has all the meta tags in the blog post head": {
-      "1": [
-        "<meta charSet=\"utf-8\"/",
-        "<meta http-equiv=\"x-ua-compatible\" content=\"ie=edge\"/",
-        "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\"/",
-        "<meta name=\"generator\" content=\"Gatsby 2.21.11\"/",
-        "<meta data-react-helmet=\"true\" name=\"description\" content=\"A guide to get started with AST (Abstract Syntax Tree) and custom built ESLint plugins\"/",
-        "<meta data-react-helmet=\"true\" name=\"image\" content=\"https://blog.maximeheckel.com/opengraph-images/how-i-built-my-first-custom-esl-int-rule.png\"/",
-        "<meta data-react-helmet=\"true\" property=\"og:url\" content=\"https://blog.maximeheckel.com/posts/how-to-build-first-eslint-rule/\"/",
-        "<meta data-react-helmet=\"true\" property=\"og:type\" content=\"article\"/",
-        "<meta data-react-helmet=\"true\" name=\"keywords\" content=\"Software Engineer, React, Redux, Typescript, Jest, Kubernetes, Docker, Entreprise, Frontend, Engineering, Blog\"/",
-        "<meta data-react-helmet=\"true\" property=\"og:title\" content=\"How I built my first custom ESLint rule - Maxime Heckel&#x27;s Blog\"/",
-        "<meta data-react-helmet=\"true\" property=\"og:description\" content=\"A guide to get started with AST (Abstract Syntax Tree) and custom built ESLint plugins\"/",
-        "<meta data-react-helmet=\"true\" property=\"og:image\" content=\"https://blog.maximeheckel.com/opengraph-images/how-i-built-my-first-custom-esl-int-rule.png\"/",
-        "<meta data-react-helmet=\"true\" name=\"twitter:card\" content=\"summary",
-        "<meta data-react-helmet=\"true\" name=\"twitter:creator\" content=\"@MaximeHeckel\"/",
-        "<meta data-react-helmet=\"true\" name=\"twitter:site\" content=\"@MaximeHeckel\"/",
-        "<meta data-react-helmet=\"true\" name=\"twitter:title\" content=\"How I built my first custom ESLint rule - Maxime Heckel&#x27;s Blog\"/",
-        "<meta data-react-helmet=\"true\" name=\"twitter:description\" content=\"A guide to get started with AST (Abstract Syntax Tree) and custom built ESLint plugins\"/",
-        "<meta data-react-helmet=\"true\" name=\"twitter:image\" content=\"https://blog.maximeheckel.com/opengraph-images/how-i-built-my-first-custom-esl-int-rule.png\"/",
-        "<meta name=\"theme-color\" content=\"#ffffff\"/"
-      ],
-      "2": [
-        "rel=\"canonical\" href=\"https://blog.maximeheckel.com/posts/how-to-build-first-eslint-rule/\"/"
-      ]
-    },
     "has all the meta tags and the expected canonical url set in the landing page head": {
       "1": [
         "<meta charSet=\"utf-8\"/",
         "<meta http-equiv=\"x-ua-compatible\" content=\"ie=edge\"/",
         "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\"/",
         "<meta name=\"generator\" content=\"Gatsby 2.21.11\"/",
-        "<meta data-react-helmet=\"true\" name=\"description\" content=\"Hi I&#x27;m Maxime, I write about React, Redux, JAMStack, Typescript, GraphQL, Serverless, Testing and many other things!\"/",
-        "<meta data-react-helmet=\"true\" name=\"image\" content=\"\"/",
+        "<meta data-react-helmet=\"true\" name=\"description\" content=\"Hi I&#x27;m Maxime, and this is my blog. Here, I share through my writing my experience as a frontend engineer and everything I&#x27;m learning about on React, Typescript, SwiftUI, Serverless, and testing.\"/",
+        "<meta data-react-helmet=\"true\" name=\"image\" content=\"https://blog.maximeheckel.com/main-og-image.png\"/",
         "<meta data-react-helmet=\"true\" property=\"og:url\" content=\"https://blog.maximeheckel.com/\"/",
-        "<meta data-react-helmet=\"true\" name=\"keywords\" content=\"Software Engineer, React, Redux, Typescript, Jest, Kubernetes, Docker, Entreprise, Frontend, Engineering, Blog\"/",
+        "<meta data-react-helmet=\"true\" name=\"keywords\" content=\"Software Engineer, SwiftUI, React, Redux, Typescript, Jest, Kubernetes, Docker, Entreprise, Frontend, Engineering, Blog, iOS, native\"/",
         "<meta data-react-helmet=\"true\" property=\"og:title\" content=\"Maxime Heckel&#x27;s Blog\"/",
-        "<meta data-react-helmet=\"true\" property=\"og:description\" content=\"Hi I&#x27;m Maxime, I write about React, Redux, JAMStack, Typescript, GraphQL, Serverless, Testing and many other things!\"/",
+        "<meta data-react-helmet=\"true\" property=\"og:description\" content=\"Hi I&#x27;m Maxime, and this is my blog. Here, I share through my writing my experience as a frontend engineer and everything I&#x27;m learning about on React, Typescript, SwiftUI, Serverless, and testing.\"/",
+        "<meta data-react-helmet=\"true\" property=\"og:image\" content=\"https://blog.maximeheckel.com/main-og-image.png\"/",
         "<meta data-react-helmet=\"true\" name=\"twitter:card\" content=\"summary",
         "<meta data-react-helmet=\"true\" name=\"twitter:creator\" content=\"@MaximeHeckel\"/",
         "<meta data-react-helmet=\"true\" name=\"twitter:site\" content=\"@MaximeHeckel\"/",
         "<meta data-react-helmet=\"true\" name=\"twitter:title\" content=\"Maxime Heckel&#x27;s Blog\"/",
-        "<meta data-react-helmet=\"true\" name=\"twitter:description\" content=\"Hi I&#x27;m Maxime, I write about React, Redux, JAMStack, Typescript, GraphQL, Serverless, Testing and many other things!\"/",
+        "<meta data-react-helmet=\"true\" name=\"twitter:description\" content=\"Hi I&#x27;m Maxime, and this is my blog. Here, I share through my writing my experience as a frontend engineer and everything I&#x27;m learning about on React, Typescript, SwiftUI, Serverless, and testing.\"/",
+        "<meta data-react-helmet=\"true\" name=\"twitter:image\" content=\"https://blog.maximeheckel.com/main-og-image.png\"/",
         "<meta name=\"theme-color\" content=\"#ffffff\"/"
       ],
       "2": [
@@ -134,7 +35,7 @@ module.exports = {
         "<meta data-react-helmet=\"true\" name=\"image\" content=\"https://blog.maximeheckel.com/opengraph-images/how-i-built-my-first-custom-esl-int-rule.png\"/",
         "<meta data-react-helmet=\"true\" property=\"og:url\" content=\"https://blog.maximeheckel.com/posts/how-to-build-first-eslint-rule/\"/",
         "<meta data-react-helmet=\"true\" property=\"og:type\" content=\"article\"/",
-        "<meta data-react-helmet=\"true\" name=\"keywords\" content=\"Software Engineer, React, Redux, Typescript, Jest, Kubernetes, Docker, Entreprise, Frontend, Engineering, Blog\"/",
+        "<meta data-react-helmet=\"true\" name=\"keywords\" content=\"Software Engineer, SwiftUI, React, Redux, Typescript, Jest, Kubernetes, Docker, Entreprise, Frontend, Engineering, Blog, iOS, native\"/",
         "<meta data-react-helmet=\"true\" property=\"og:title\" content=\"How I built my first custom ESLint rule - Maxime Heckel&#x27;s Blog\"/",
         "<meta data-react-helmet=\"true\" property=\"og:description\" content=\"A guide to get started with AST (Abstract Syntax Tree) and custom built ESLint plugins\"/",
         "<meta data-react-helmet=\"true\" property=\"og:image\" content=\"https://blog.maximeheckel.com/opengraph-images/how-i-built-my-first-custom-esl-int-rule.png\"/",
@@ -150,5 +51,6 @@ module.exports = {
         "rel=\"canonical\" href=\"https://blog.maximeheckel.com/posts/how-to-build-first-eslint-rule/\"/"
       ]
     }
-  }
+  },
+  "__version": "5.4.0"
 }
