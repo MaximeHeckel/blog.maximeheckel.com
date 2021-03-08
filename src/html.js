fd6ee1597a0a95ff073103b2b64ff45c525bc178
Maxime <heckelmaxime@gmail.com>
2021-03-07 19:25:01 -0500

    docs: Scrollspy demystified

--- src/html.js
+++ src/html.js
@@ -25,7 +25,7 @@ export default function HTML(props) {
         />
         {props.headComponents}
       </head>
-      <body {...props.bodyAttributes}>
+      <body {...props.bodyAttributes} className="maximeheckel-light">
         <script
           key="maximeheckel-theme"
           dangerouslySetInnerHTML={{
