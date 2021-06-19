b7f378da76e959a1c65c49acb29c6420832be577
Maxime Heckel <heckelmaxime@gmail.com>
2021-06-18 23:28:24 -0400

    chore: Upgrade to Next.js 11 (#367)

---
+++ scripts/generate-opengraph-images.js
@@ -0,0 +1,84 @@
+/* eslint-disable no-console */
+// https://phiilu.com/generate-open-graph-images-for-your-static-next-js-site
+const playwright = require('playwright-aws-lambda');
+const { createHash } = require('crypto');
+const fs = require('fs');
+const matter = require('gray-matter');
+const path = require('path');
+const qs = require('qs');
+
+const ogImageDir = `./public/static/og`;
+
+(async () => {
+  const root = process.cwd();
+
+  if (process.env.NODE_ENV === 'development') {
+    return 'opengraph images will only be generated in production build';
+  }
+
+  const files = fs.readdirSync(path.join(root, 'content'));
+
+  const posts = files.reduce((allPosts, postSlug) => {
+    const source = fs.readFileSync(
+      path.join(root, 'content', postSlug),
+      'utf8'
+    );
+    const { data } = matter(source);
+    return [
+      {
+        ...data,
+        slug: postSlug.replace('.mdx', ''),
+      },
+      ...allPosts,
+    ];
+  }, []);
+
+  for (let index = 0; index < posts.length; index++) {
+    const post = posts[index];
+    const hash = createHash('md5').update(post.title).digest('hex');
+    const imagePath = `${ogImageDir}/${hash}.png`;
+
+    const params = {
+      title: post.title,
+      background: post.colorFeatured,
+      color: post.fontFeatured,
+    };
+
+    const filteredParams = Object.keys(params).reduce((acc, curr) => {
+      if (!params[curr] || params[curr] === 'undefined') {
+        return acc;
+      }
+      return {
+        ...acc,
+        [curr]: params[curr],
+      };
+    }, {});
+
+    const url = `https://og-image-maximeheckel.netlify.app/ogimage?${qs.stringify(
+      filteredParams
+    )}`;
+
+    try {
+      fs.statSync(imagePath);
+    } catch (error) {
+      console.info(`[OG IMAGE]: Generating opengraph image for ${post.title}`);
+
+      try {
+        const browser = await playwright.launchChromium({ headless: true });
+
+        const page = await browser.newPage();
+        await page.setViewportSize({ width: 1200, height: 630 });
+        await page.goto(url, { waitUntil: 'networkidle' });
+
+        const buffer = await page.screenshot({ type: 'png' });
+        await browser.close();
+
+        fs.mkdirSync(ogImageDir, { recursive: true });
+        fs.writeFileSync(imagePath, buffer);
+      } catch (error) {
+        console.error(error);
+        process.exit(1);
+      }
+    }
+  }
+})();
