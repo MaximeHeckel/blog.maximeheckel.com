d2f9e48f214ea9fa8d60ecdb339c6d8b3ee922dc
Maxime Heckel <heckelmaxime@gmail.com>
2022-09-11 11:39:07 -0400

    fix(widget): Improve uv wrapping around blob sphere in shader article

--- core/components/MDX/Widgets/ShaderReactThreeFiber/blob.ts
+++ core/components/MDX/Widgets/ShaderReactThreeFiber/blob.ts
@@ -94,7 +94,7 @@ varying float vDisplacement;
 void main() {
   float distort = 2.0 * vDisplacement * u_intensity;
 
-  vec3 color = vec3(vUv * (1.0 - distort), 1.0);
+  vec3 color = vec3(abs(vUv - 0.5) * 2.0  * (1.0 - distort), 1.0);
   
   gl_FragColor = vec4(color ,1.0);
 }
