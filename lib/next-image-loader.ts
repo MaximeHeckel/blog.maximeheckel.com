957f0a7a8e99717fc117ab799786d3563b48c8b9
Maxime <heckelmaxime@gmail.com>
2024-02-09 17:40:50 -0500

    chore: Update cloudinary URLs to load lighter images

--- lib/next-image-loader.ts
+++ lib/next-image-loader.ts
@@ -1,5 +1,5 @@
 import { ImageLoaderProps } from 'next/legacy/image';
 
-export const loader = ({ src, quality }: ImageLoaderProps) => {
-  return `https://res.cloudinary.com/dg5nsedzw/image/upload/q_${quality}/${src}`;
+export const loader = ({ src }: ImageLoaderProps) => {
+  return `https://res.cloudinary.com/dg5nsedzw/image/upload/fl_lossy,f_auto,q_auto/${src}`;
 };
