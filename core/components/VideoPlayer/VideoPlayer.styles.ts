e2e5067667b74d09bb15e0f1076d39d988e3aba9
Maxime Heckel <heckelmaxime@gmail.com>
2024-08-08 17:43:19 -0400

    fix: Fix video player CLS (#692)

---
+++ core/components/VideoPlayer/VideoPlayer.styles.ts
@@ -0,0 +1,20 @@
+import { styled } from '@maximeheckel/design-system';
+
+export const Video = styled('video', {
+  width: '100%',
+  height: '100%',
+  display: 'block',
+  objectFit: 'cover',
+});
+
+export const VideoContainer = styled('figure', {
+  position: 'relative',
+  margin: '0 auto',
+  background: 'var(--emphasis)',
+  borderRadius: 'var(--border-radius-3)',
+  border: '2px solid var(--border-color)',
+  width: '100%',
+  height: '100%',
+  overflow: 'hidden',
+  transform: 'scale(1) translateZ(0)',
+});
--- core/components/VideoPlayer/VideoPlayer.tsx
+++ core/components/VideoPlayer/VideoPlayer.tsx
@@ -1,5 +1,7 @@
-import { styled, useTheme } from '@maximeheckel/design-system';
-import React from 'react';
+import { useDebouncedValue } from '@maximeheckel/design-system';
+import { useInView, useReducedMotion } from 'framer-motion';
+import React, { useRef } from 'react';
+import * as S from './VideoPlayer.styles';
 
 interface VideoPlayerProps {
   autoPlay?: boolean;
@@ -9,56 +11,50 @@ interface VideoPlayerProps {
   muted?: boolean;
   width?: number;
   height?: number;
+  alt?: string;
   src: string;
 }
 
-const StyledVideo = styled('video', {
-  margin: '0 auto',
-  background: 'var(--emphasis)',
-  borderRadius: 'var(--border-radius-3)',
-  border: '4px solid var(--border-color)',
-  maxWidth: '100%',
-  height: 'auto',
-});
-
-const getDisplayedPoster = (poster: string, dark: boolean) => {
-  if (dark) {
-    return `/static/posters/${poster}_dark.png`;
-  }
-
-  return `/static/posters/${poster}_light.png`;
-};
-
 const VideoPlayer = (props: VideoPlayerProps) => {
-  const { autoPlay, controls, loop, muted, width, height, poster, src } = props;
-  const { dark } = useTheme();
-  const [currentPoster, setCurrentPoster] = React.useState<string | undefined>(
-    undefined
-  );
+  const {
+    alt,
+    autoPlay,
+    controls = false,
+    loop,
+    muted,
+    width,
+    height,
+    src,
+  } = props;
 
-  React.useEffect(() => {
-    if (poster) {
-      if (!poster.includes('.png') && !poster.includes('https')) {
-        setCurrentPoster(getDisplayedPoster(poster, dark));
-      } else {
-        setCurrentPoster(poster);
-      }
-    }
-  }, [dark, poster]);
+  const ref = useRef(null);
+  const isReducedMotionEnabled = useReducedMotion();
+  const isInView = useInView(ref, {
+    amount: 0.1,
+  });
+
+  const debouncedIsInView = useDebouncedValue(isInView, 200);
 
   return (
-    <StyledVideo
-      autoPlay={autoPlay}
-      poster={currentPoster}
-      width={width}
-      height={height}
-      controls={controls}
-      loop={loop || false}
-      muted={muted}
-      playsInline
+    <S.VideoContainer
+      ref={ref}
+      css={{
+        aspectRatio: `${width} / ${height}`,
+      }}
     >
-      <source src={src} type="video/mp4" />
-    </StyledVideo>
+      {debouncedIsInView ? (
+        <S.Video
+          aria-label={alt}
+          autoPlay={autoPlay}
+          controls={controls || isReducedMotionEnabled || false}
+          loop={loop || isReducedMotionEnabled || false}
+          muted={muted}
+          playsInline
+        >
+          <source src={src} type="video/mp4" />
+        </S.Video>
+      ) : null}
+    </S.VideoContainer>
   );
 };
 
