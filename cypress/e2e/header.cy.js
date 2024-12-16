cf1e46a9851c7b2897bc8184298027e1506ce0cd
Maxime <heckelmaxime@gmail.com>
2024-12-16 22:16:17 +0100

    chore: Fix tests

--- cypress/e2e/header.cy.js
+++ cypress/e2e/header.cy.js
@@ -1,7 +1,7 @@
 describe('Header Tests', () => {
   it('The post title is visible when navigating to a project and scrolling', () => {
     cy.visit('/posts/how-to-build-first-eslint-rule/');
-    cy.scrollTo(0, 500);
+    cy.scrollTo(0, 2000);
     cy.get('[data-testid="header-title"]').should('be.visible').click();
     cy.wait(1000).then(() => {
       cy.get('[data-testid="header-title"]').should('not.be.visible');
