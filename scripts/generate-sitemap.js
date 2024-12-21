a0f74372150f7cbd523e8ded9e80e718877bcc00
Maxime <heckelmaxime@gmail.com>
2024-12-21 18:16:43 +0100

    fix: Fix sitemap.xml formatting issue

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -16,29 +16,26 @@ import { globby } from 'globby';
     '!pages/404.tsx',
   ]);
 
-  const sitemap = `
-        <?xml version="1.0" encoding="UTF-8"?>
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
-            ${pages
-              .map((page) => {
-                const path = page
-                  .replace('pages', '')
-                  .replace('content', '/posts')
-                  .replace('.js', '')
-                  .replace('.tsx', '')
-                  .replace('.ts', '')
-                  .replace('.mdx', '');
-                const route = path === '/index' ? '' : path;
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+      ${pages
+        .map((page) => {
+          const path = page
+            .replace('pages', '')
+            .replace('content', '/posts')
+            .replace('.js', '')
+            .replace('.tsx', '')
+            .replace('.ts', '')
+            .replace('.mdx', '');
+          const route = path === '/index' ? '' : path;
 
-                return `
-                        <url>
-                            <loc>${`https://blog.maximeheckel.com${route}/`}</loc>
-                            <changefreq>daily</changefreq>
-                            <priority>0.7</priority>
-                        </url>
-                    `;
-              })
-              .join('')}
+          return `<url>
+  <loc>${`https://blog.maximeheckel.com${route}/`}</loc>
+  <changefreq>daily</changefreq>
+  <priority>0.7</priority>
+</url>`;
+        })
+        .join('')}
         </urlset>
     `;
 
