a617f94b0a6d0c7b1fc8824c7f4c8fa58bcd461b
Maxime Heckel <heckelmaxime@gmail.com>
2026-02-08 12:48:52 -0500

    fix: Fix autolink-header not copying urls

--- core/features/BlogPost/BlogPost.tsx
+++ core/features/BlogPost/BlogPost.tsx
@@ -209,6 +209,28 @@ const BlogPost = ({ children, frontMatter, ogImage }: Props) => {
     }, 500);
   }, [slug]);
 
+  useEffect(() => {
+    const handleAnchorClick = (e: MouseEvent) => {
+      const target = e.currentTarget as HTMLAnchorElement;
+      const href = target.getAttribute('href');
+      if (href) {
+        const fullUrl = `${postUrl}${href}`;
+        navigator.clipboard.writeText(fullUrl);
+      }
+    };
+
+    const anchorLinks = document.querySelectorAll('.anchor-link');
+    anchorLinks.forEach((link) => {
+      link.addEventListener('click', handleAnchorClick as EventListener);
+    });
+
+    return () => {
+      anchorLinks.forEach((link) => {
+        link.removeEventListener('click', handleAnchorClick as EventListener);
+      });
+    };
+  }, [postUrl, ids]);
+
   return (
     <Main>
       <Seo
