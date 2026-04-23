242b38e06d3bedc2464793147f9dbe97a7ec75e2
Maxime Heckel <heckelmaxime@gmail.com>
2026-04-23 13:29:17 +0000

    fix: Patch image path

--- lib/next-image-loader.ts
+++ lib/next-image-loader.ts
@@ -1,6 +1,8 @@
 import { ImageLoaderProps } from 'next/legacy/image';
 
 export const cloudflareLoader = ({ src, width, quality }: ImageLoaderProps) => {
+  // @ts-ignore
   const params = [`width=${width}`, `quality=${quality || 75}`, 'format=webp'];
-  return `https://images.maximeheckel.com/cdn-cgi/image/${params.join(',')}/https://cdn.maximeheckel.com/images/${src}`;
+ // return `https://images.maximeheckel.com/cdn-cgi/image/${params.join(',')}/https://cdn.maximeheckel.com/images/${src}`;
+  return `https://cdn.maximeheckel.com/images/${src}`;
 };
