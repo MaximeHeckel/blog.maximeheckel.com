0ecc7cbca0666812aa0f59b40287d8cbcfa3a98f
Maxime Heckel <heckelmaxime@gmail.com>
2026-04-23 15:15:06 +0000

    fix: New asset transformation endpoint

--- lib/next-image-loader.ts
+++ lib/next-image-loader.ts
@@ -2,7 +2,7 @@ import { ImageLoaderProps } from 'next/legacy/image';
 
 export const cloudflareLoader = ({ src, width, quality }: ImageLoaderProps) => {
   // @ts-ignore
-  const params = [`width=${width}`, `quality=${quality || 75}`, 'format=webp'];
+  const params = [`w=${width}`, `q=${quality || 75}`];
  // return `https://images.maximeheckel.com/cdn-cgi/image/${params.join(',')}/https://cdn.maximeheckel.com/images/${src}`;
-  return `https://cdn.maximeheckel.com/images/${src}`;
+  return `https://assets.maximeheckel.com/images/${src}?${params.join('&')}`;
 };
